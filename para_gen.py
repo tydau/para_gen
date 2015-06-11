e24073d93704b1f49a361f53215f951cadffa651
T. Tyler Daugherty <tydau@uga.edu>
2015-06-11 15:48:38 -0400

    Added return to original start point in para

--- para_gen.py
+++ para_gen.py
@@ -17,8 +17,11 @@ end = float(raw_input("Ending point:\n"))
 int = float(raw_input("Inverval:\n"))
 name = 'cellindent.txt'
 
-para_vals = list(np.arange(start,end,int))
+para_vals = list(np.arange(start,end+int,int))
+parra_vals_rev = list(reversed(np.arange(start,end,int)))
+para_vals.append(parra_vals_rev)
 para_vals = str(para_vals).strip('[]')
+para_vals = para_vals.replace("[","")
 
 file = open(name,'w')
 file.write(para_vals)
