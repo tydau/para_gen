2f599f7fd2dd8f85a692e178a6d8c8662a2eb429
T. Tyler Daugherty <tydau@uga.edu>
2015-06-11 16:02:50 -0400

    Multiple line output algorithm

--- para_gen.py
+++ para_gen.py
@@ -15,15 +15,17 @@ import numpy as np
 start = float(raw_input("Starting point:\n"))
 end = float(raw_input("Ending point:\n"))
 int = float(raw_input("Inverval:\n"))
-name = 'cellindent.txt'
+name_fwd = 'cellindent_fwd.txt'
+name_rev = 'cellindent_rev.txt'
 
 para_vals = list(np.arange(start,end+int,int))
 parra_vals_rev = list(reversed(np.arange(start,end+int,int)))
 para_vals = str(para_vals).strip('[]')
 parra_vals_rev = str(parra_vals_rev).strip('[]')
 
-file = open(name,'w')
-file.write(para_vals)
-file.write('\n')
-file.write(parra_vals_rev)
-file.close()
+file_fwd = open(name_fwd,'w')
+file_rev = open(name_rev,'w')
+file_fwd.write(para_vals)
+file_rev.write(parra_vals_rev)
+file_fwd.close()
+file_rev.close()
